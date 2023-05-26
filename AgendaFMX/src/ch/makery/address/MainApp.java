ec822772378cdb4e104024e4ff2f1c7fe3b695ac
Andre <72696560@continental.edu.pe>
2023-05-25 19:06:34 -0500

    Se corrigio algunos errores y se llego al punto 2

--- AgendaFMX/src/ch/makery/address/MainApp.java
+++ AgendaFMX/src/ch/makery/address/MainApp.java
@@ -1,8 +1,12 @@
 package ch.makery.address;
 
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -13,7 +17,38 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+// ... AFTER THE OTHER VARIABLES ...
 
+	/**
+	 * The data as an observable list of Persons.
+	 */
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+	/**
+	 * Constructor
+	 */
+	public MainApp() {
+		// Add some sample data
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Meier"));
+		personData.add(new Person("Martin", "Mueller"));
+	}
+  
+	/**
+	 * Returns the data as an observable list of Persons. 
+	 * @return
+	 */
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
+  
+    // ... THE REST OF THE CLASS ...
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -44,17 +79,22 @@ public class MainApp extends Application {
     }
 
     /**
-     * Shows the person overview inside the root layout.
-     */
+ * Shows the person overview inside the root layout.
+ */
     public void showPersonOverview() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
