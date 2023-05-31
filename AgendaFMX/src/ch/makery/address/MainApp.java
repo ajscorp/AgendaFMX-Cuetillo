fd16aad8a915039d7f6e082ea259af2089ab9c45
Andre <72696560@continental.edu.pe>
2023-05-30 23:35:08 -0500

    Se agrego el icono y se intento finalizar la parte hasta parte 5 pero aun no se puede

--- AgendaFMX/src/ch/makery/address/MainApp.java
+++ AgendaFMX/src/ch/makery/address/MainApp.java
@@ -3,7 +3,9 @@ package ch.makery.address;
 import ch.makery.address.model.Person;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -156,4 +158,105 @@ public void start(Stage primaryStage)
     public static void main(String[] args) {
         launch(args);
     }
+    /**
+    * Returns the person file preference, i.e. the file that was last opened.
+    * The preference is read from the OS specific registry. If no such
+    * preference can be found, null is returned.
+    * 
+    * @return
+    */
+   public File getPersonFilePath() {
+       Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+       String filePath = prefs.get("filePath", null);
+       if (filePath != null) {
+           return new File(filePath);
+       } else {
+           return null;
+       }
+   }
+
+   /**
+    * Sets the file path of the currently loaded file. The path is persisted in
+    * the OS specific registry.
+    * 
+    * @param file the file or null to remove the path
+    */
+   public void setPersonFilePath(File file) {
+       Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+       if (file != null) {
+           prefs.put("filePath", file.getPath());
+
+           // Update the stage title.
+           primaryStage.setTitle("AddressApp - " + file.getName());
+       } else {
+           prefs.remove("filePath");
+
+           // Update the stage title.
+           primaryStage.setTitle("AddressApp");
+       }
+   }
+
+    public void loadPersonDataFromFile(File file) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void savePersonDataToFile(File personFile) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   /*
+    * Loads person data from the specified file. The current person data will
+    * be replaced.
+    * 
+    * @param file
+    *//*
+   public void loadPersonDataFromFile(File file) {
+       try {
+           JAXBContext context = JAXBContext
+                   .newInstance(PersonListWrapper.class);
+           Unmarshaller um = context.createUnmarshaller();
+
+           // Reading XML from the file and unmarshalling.
+           PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+           personData.clear();
+           personData.addAll(wrapper.getPersons());
+
+           // Save the file path to the registry.
+           setPersonFilePath(file);
+
+       } catch (Exception e) { // catches ANY exception
+           Dialogs.create()
+                   .title("Error")
+                   .masthead("Could not load data from file:\n" + file.getPath())
+                   .showException(e);
+       }
+   }
+*/
+   /**
+    * Saves the current person data to the specified file.
+    * 
+    * @param file
+    *//*
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Dialogs.create().title("Error")
+                    .masthead("Could not save data to file:\n" + file.getPath())
+                    .showException(e);
+        }
+    }*/
 }
